0981abdcd7e32207baa76f1f497a48fd9dede948
Daria <tkachenko.darya95@gmail.com>
2026-01-06 14:10:47 +0200

    Initial commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+
+interface MoviesResponse {
+  results: Movie[];
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const token = import.meta.env.VITE_TMDB_TOKEN;
+
+  const response = await axios.get<MoviesResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  return response.data.results;
+};
+
+
